a84623f569a062aacce062b775a584d14c7d0b9b
Ben Smith <benedictsmith@gmail.com>
2015-01-11 05:20:06 +0000

    added categories

--- src/app.jsx
+++ src/app.jsx
@@ -2,57 +2,61 @@ var React = require('react');
 var Reflux = require('reflux');
 var Firebase = require("firebase");
 var Router = require('react-router');
-
+var _ = require('underscore');
+//var css = require("css!./materialize.min.css");
 
 var UserActions = Reflux.createActions([
   "login",
   "logout"
-  ])
+])
 
-  var fb = new Firebase("https://brecon.firebaseio.com/");
+var fb = new Firebase("https://brecon.firebaseio.com/");
 
-  fb.onAuth(function(authData) {
-    console.log('auth data seen')
-    console.log(authData)
-    if (authData) {
-      fb.child("users").child(authData.uid).set(authData);
-    }
-  });
+fb.onAuth(function(authData) {
+  console.log('auth data seen')
+  console.log(authData)
+  if (authData) {
+        fb.child("users").child(authData.uid).set(authData);
+  }
+});
 
-  var UserStore = Reflux.createStore({
-    listenables: [UserActions],
+//fb.child('categories').push({title: 'Gastroenterology'});
+//fb.child('categories').push({title: 'Dermatology'});
 
-    init: function() {
-      this.user = {};
-    },
+var UserStore = Reflux.createStore({
+  listenables: [UserActions],
 
-    login: function(provider) {
-      fb.authWithOAuthPopup(provider, function(data) {
-        console.log(data)
-      })
-    },
+  init: function() {
+    this.user = {};
+  },
 
-    logout: function() {
-      fb.unauth()
-    }
+  login: function(provider) {
+    fb.authWithOAuthPopup(provider, function(data) {
+      console.log(data)
+    })
+  },
 
-  })
+  logout: function() {
+    fb.unauth()
+  }
 
-  var Header = React.createClass({ // needs user details
-    getDefaultProps: function() {
-      return ({loggedIn : false})
-    },
+})
 
-    render: function() {
-      var loginOrOut = this.props.loggedIn ?
+var Header = React.createClass({ // needs user details
+  getDefaultProps: function() {
+    return ({loggedIn : false})
+  },
+
+  render: function() {
+    var loginOrOut = this.props.loggedIn ?
       <Router.Link to="profile">Profile</Router.Link> :
       <Router.Link to="logout">Sign in</Router.Link>;
 
-      return (
+    return (
         <header>
         <nav>
         <div className="nav-wrapper blue-grey">
-        <a href="#" className="brand-logo">CME Community</a>
+        <a href="/" className="brand-logo"><i className="mdi-maps-local-library"></i>CME Community</a>
 
         <ul id="nav-mobile" className="right side-nav">
         <li>{ loginOrOut }</li>
@@ -64,101 +68,125 @@ var UserActions = Reflux.createActions([
     })
 
 
-    var Main = React.createClass({
-      render: function() {
-        // first time flag
-        return (
-          <h3>Start on your CME</h3>
+var Main = React.createClass({
+  render: function() {
+    // first time flag
+    return (
+      <div>
+      <h3>Start on your CME</h3>
+      <CategoryList />
+      </div>
+    )
+  }
+})
 
-        )
-      }
+var CategoryList = React.createClass({
+  componentWillMount: function() {
+    var self = this;
+    //var ref = new Firebase("https://brecon.firebaseio.com/")
+    fb.child('categories').on('value', function(snapshot) {
+      self.replaceState({categories: snapshot.val()})
+    });
+
+  },
+  getInitialState: function() {
+    return {categories: {}}
+  },
+  render: function() {
+    var categories = _.map(this.state.categories, function(c, i, v) {
+      return (<Category key={i} {...c} />)
     })
+    return (<ul>{ categories }</ul>)
+  }
+})
 
+// needs slug
 
+var Category = React.createClass({
+  render: function() {
+    return (<li>{ this.props.title }</li>)
+  }
+})
 
-    var Login = React.createClass({
-      handleLogin: function(provider) {
-        UserActions.login(provider);
-      },
-      render: function() {
-        return (<div className="row">
-        <div className="col s12">
-        <div className="valign-demo valign-wrapper">
-        <h5>Please login to begin</h5>
-        <img onClick={this.handleLogin('twitter')} src='images/sign-in-with-twitter-gray.png' />
-        <button onClick={this.handleLogin('facebook')}>Facebook</button>
+var Login = React.createClass({
+  handleLogin: function(provider) {
+    UserActions.login(provider);
+  },
+  render: function() {
+    return (<div className="row">
+    <div className="col s12">
+    <div className="valign-demo valign-wrapper">
+    <h5>Please login to begin</h5>
+    <img onClick={this.handleLogin('twitter')} src='images/sign-in-with-twitter-gray.png' />
+    <button onClick={this.handleLogin('facebook')}>Facebook</button>
 
+    </div>
+
+    </div>
+    </div>)
+  }
+})
+
+var Profile = React.createClass({
+  render: function() {
+    return (<div>This could be your profile</div>)
+  }
+})
+
+var Footer = React.createClass({
+  render: function() {
+    return (
+      <footer className="blue-grey lighten-3" >
+      <div className="footer-copyright">
+      <div className="container">
+      Proudly made in Wien
+      </div>
+      </div>
+      </footer>)
+    }
+  })
+
+  var App = React.createClass({
+    mixins: [Reflux.connect(UserStore, this.replaceState)],
+    getInitialState: function() {
+      return {loggedIn: true}
+    },
+
+    render: function() {
+      var main = this.state.loggedIn ? <Router.RouteHandler /> : <Login />
+
+      return (
+        <div>
+        <Header />
+        <main>
+        <div className="container">
+        <div className="row">{ main }</div>
         </div>
+        </main>
 
+        <Footer />
         </div>
-        </div>)
-      }
-    })
+      )
+    }
+  })
 
-    var Profile = React.createClass({
-      render: function() {
-        return (<div>This could be your profile</div>)
-      }
-    })
+var Logout = React.createClass({
+  componentDidMount: function () {
+    UserActions.logout();
+  },
 
-    var Footer = React.createClass({
-      render: function() {
-        return (
-          <footer className="blue-grey lighten-3" >
-          <div className="footer-copyright">
-          <div className="container">
-          Proudly made in Wien
-          </div>
-          </div>
-          </footer>)
-        }
-      })
+  render: function () {
+    return <p>You are now logged out</p>;
+  }
+});
 
-      var App = React.createClass({
-        mixins: [Reflux.connect(UserStore)],
-        getInitialState: function() {
-          return {loggedIn: true}
-        },
-
-        handleClick: function() {
-          console.log('clicked')
-          registry.course.pages.add({title: 'test'})
-        },
-        render: function() {
-          var main = this.state.loggedIn ? <Router.RouteHandler /> : <Login />
-
-          return (
-            <div>
-            <Header />
-            <main>
-            <div className="container">
-            <div className="row">{ main }</div>
-            </div>
-            </main>
-
-            <Footer />
-            </div>
-          )
-        }
-      })
-
-      var Logout = React.createClass({
-        componentDidMount: function () {
-          UserActions.logout();
-        },
-
-        render: function () {
-          return <p>You are now logged out</p>;
-        }
-      });
-
-      var routes = (
-        <Router.Route handler={App}>
-        <Router.DefaultRoute handler={Main} />
-        <Router.Route name='profile' handler={Profile} />
-        <Router.Route name='logout' handler={Logout} />
-        </Router.Route>
-      );
+var routes = (
+  <Router.Route handler={App}>
+  <Router.DefaultRoute handler={Main} />
+  <Router.Route name='profile' handler={Profile} />
+  <Router.Route name='logout' handler={Logout} />
+  </Router.Route>
+);
 
 
 
@@ -170,4 +198,3 @@ var UserActions = Reflux.createActions([
       //<Router.Route name="categories" path="c/?" handler={CourseList} />,
 
       //React.render(<App />, document.body);
-      
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  entry: './app.jsx',
+  entry: './src/app.jsx',
   output: {
     filename: 'dist/bundle.js'
   },
   module: {
     loaders: [
-  { test: /\.jsx/, loader: 'jsx-loader?harmony' }
+    { test: /\.jsx/, loader: 'jsx-loader?harmony' },
+    /*
+    { test: /\.html$/i, loader: "html" },
+    { test: /\.css$/i, loader: "css" },
+  { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+  loader: "file" }
+  */
   ]
 }
 };
