5c9ac6ea072a8c34168f0eff109f4960488076d6
Ben Smith <benedictsmith@gmail.com>
2015-01-11 07:09:57 +0000

    added high scores

--- src/app.jsx
+++ src/app.jsx
@@ -7,27 +7,49 @@ var _ = require('underscore');
 
 var UserActions = Reflux.createActions([
   "login",
-  "logout"
+  "logout",
+  "loggedIn"
 ])
-
+/*
+UserActions.login.listen = function(provider) {
+  fb.authWithOAuthPopup(provider, function(data) {
+    console.log(data)
+  })
+}
+*/
 var fb = new Firebase("https://brecon.firebaseio.com/");
+// switch to a transaction - https://gist.github.com/anantn/4323967
 
 fb.onAuth(function(authData) {
   console.log('auth data seen')
   console.log(authData)
   if (authData) {
-        fb.child("users").child(authData.uid).set(authData);
+    //fb.child('users').child(authData.id).once('value', function(userSnapshot) {
+      //if (!userSnapshot) { // they've been here before
+      fb.child("users").child(authData.uid).set(authData);
+    //  }
+    //})
+    UserActions.loggedIn(authData)
   }
 });
 
 //fb.child('categories').push({title: 'Gastroenterology'});
 //fb.child('categories').push({title: 'Dermatology'});
+/*
+fb.child('questions').push({category: '-JfMM91pNe-6toATwy8G', title: 'What is this?'});
+fb.child('questions').push({category: '-JfMM91pNe-6toATwy8G', title: 'What is that?'});
+*/
 
 var UserStore = Reflux.createStore({
   listenables: [UserActions],
 
   init: function() {
     this.user = {};
+  },
+
+  onLoggedIn: function(data) {
+    this.user = data;
+    this.trigger(data)
   },
 
   login: function(provider) {
@@ -89,6 +111,9 @@ var CategoryList = React.createClass({
     });
 
   },
+  componentWillUnmount: function() {
+    fb.child('categories').off('value');
+  },
   getInitialState: function() {
     return {categories: {}}
   },
@@ -100,11 +125,14 @@ var CategoryList = React.createClass({
   }
 })
 
-// needs slug
-
 var Category = React.createClass({
   render: function() {
-    return (<li><Router.Link={category} params={{slug: {this.props.slug}}}>{ this.props.title }</Router.Link></li>)
+    var slug = this.props.slug;
+    return (<li>
+      <Router.Link to='category' params={{slug: slug}}>
+      { this.props.title }
+      </Router.Link>
+      </li>)
   }
 })
 
@@ -146,29 +174,31 @@ var Footer = React.createClass({
     }
   })
 
-  var App = React.createClass({
-    mixins: [Reflux.connect(UserStore, this.replaceState)],
-    getInitialState: function() {
-      return {loggedIn: true}
-    },
+var App = React.createClass({
+  mixins: [Reflux.connect(UserStore, this.replaceState)],
 
-    render: function() {
-      var main = this.state.loggedIn ? <Router.RouteHandler /> : <Login />
+  getInitialState: function() {
+    return {loggedIn: true}
+  },
 
-      return (
-        <div>
+  render: function() {
+    var main = this.state.loggedIn ? <Router.RouteHandler /> : <Login />
+
+    return (
+      <div>
         <Header />
-        <main>
+
+      <main>
         <div className="container">
-        <div className="row">{ main }</div>
+          <div className="row">{ main }</div>
         </div>
-        </main>
+      </main>
 
         <Footer />
-        </div>
-      )
-    }
-  })
+      </div>
+    )
+  }
+})
 
 var Logout = React.createClass({
   componentDidMount: function () {
@@ -180,12 +210,94 @@ var Logout = React.createClass({
   }
 });
 
+var Question = React.createClass({
+  mixins: [Router.State],
+
+  componentWillMount: function() {
+    var self = this;
+    fb
+    .child('categories')
+    .orderByChild('slug')
+    .equalTo(this.getParams().slug)
+    .limitToFirst(1)
+    .once('value', function(snapshot) {
+      if (snapshot) {
+        console.log(_.keys(snapshot.val())[0])
+
+        var k = _.keys(snapshot.val())[0];
+
+          fb.child('question')
+            .orderByChild('category')
+            .equalTo(k)
+            .limitToFirst(1)
+            .once('value', function(question) {
+              if (question) {
+                console.log(question.val())
+                self.setState({question: _.values(question.val())[0]})
+              }
+
+            })
+          self.setState({category: snapshot.val()})
+        }
+      })
+  },
+
+  mark: function(data) {
+      console.log('mark')
+  },
+
+  getInitialState: function() {
+    return ({loading: true,
+      question: { title: '', answers: []}
+    })
+  },
+  // connect to category, then just .first()
+  // upon answer, push to user
+  render: function() {
+    var question = this.state.question.title || ''
+    var answers = this.state.answers.map(function(a) {
+      return <li onClick={this.mark}>{ a}</li>
+    })
+    return (<p>{ question } <ul>{ answers}</ul></p>)
+  }
+})
+
+var LeaderBoard = React.createClass({
+  componentWillMount: function() {
+
+    fb.child('users').orderByChild('score').limitToLast(10).on('value', function(snapshot) {
+      console.log(snapshot.val())
+    })
+
+  },
+  getInitialState: function() {
+    return ({users: []})
+  },
+  render: function() {
+    var users = this.state.users.map(function(u, k) {
+      return (<li><LeaderBoardEntry key={k} {...u} /></li>)
+    })
+
+    return (<div><h2>Top ten players</h2><ul>{ users }</ul></div>)
+
+  }
+})
+
+var LeaderBoardEntry = React.createClass({
+  render: function() {
+    return (<p>Entry goes here</p>)
+  }
+})
+
+//TODO: Remove Listeners
+
 var routes = (
   <Router.Route handler={App}>
   <Router.DefaultRoute handler={Main} />
   <Router.Route name='profile' handler={Profile} />
-  <Router.Route name='category' handler={Question} />
+  <Router.Route name='high-scores' handler={LeaderBoard} />
 
+  <Router.Route name='category' path='/category/:slug' handler={Question} />
   <Router.Route name='logout' handler={Logout} />
   </Router.Route>
 );
